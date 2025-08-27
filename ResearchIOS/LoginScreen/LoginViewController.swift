d62a45ea34c7912c7217a97c05c127f73011f733
tpthien2001 <thientp@foxcode.info>
2025-08-27 14:18:30 +0700

    first cm

--- ResearchIOS/LoginScreen/LoginViewController.swift
+++ ResearchIOS/LoginScreen/LoginViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtUserName: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func login_Clicked(_ sender: Any) {
+        let userName = txtUserName.text ?? ""
+        let password = txtPassword.text ?? ""
+        if userName.isEmpty || password.isEmpty {
+            let alertController = UIAlertController(title: "Error", message: "Please enter username and password", preferredStyle: .alert)
+            let actionOk = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(actionOk)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            
+        }
+    
     }
-    */
+    
 
 }
--- ResearchIOS/ViewController.swift
+++ ResearchIOS/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func test1_clicked(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+            if let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController {
+                navigationController?.pushViewController(loginVC, animated: true)
+            }
+    }
+    
+    @IBAction func test2_clicked(_ sender: Any) {
+            
+    }
+    
+    @IBAction func test3_clicked(_ sender: Any) {
+        
+    }
+    
+    
 }
 
