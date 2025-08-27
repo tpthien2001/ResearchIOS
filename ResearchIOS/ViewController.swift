df9acec1e1f21898ad88c25407bd86a0ff2be93d
tpthien2001 <thientp@foxcode.info>
2025-08-27 10:01:25 +0700

    Initial Commit

---
+++ ResearchIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResearchIOS
+//
+//  Created by Trần Thiện on 27/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
